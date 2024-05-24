9b01b7aa8020d7a2afda93f3e4099cf5193bb403
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-05-24 11:56:08 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+interface User {
+  img: string;
+  lastname: string;
+  firstname: string;
+  age: number;
+  citation: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'bindingAngular';
+  user: User;
+
+  constructor() {
+    this.user = {
+      img: "https://picsum.photos/200",
+      lastname: "Doe",
+      firstname: "John",
+      age: 30,
+      citation: ""
+    };
+  }
 }
